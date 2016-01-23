25c2b2a56894e75100a2f522daa18710dd7dea2c
Jit <kanetkarster@gmail.com>
2016-01-23 15:28:19 -0500

    tested more edge cases

--- fsm_tb.vhd
+++ fsm_tb.vhd
@@ -34,11 +34,11 @@ CONSTANT CHAR_B : char := "01000010";
 
 signal ALL_CODE : str := (CHAR_A, CHAR_B, CHAR_A, CHAR_A, CHAR_B, CHAR_A, CHAR_B, CHAR_A, CHAR_A);
 
-signal ALL_LN_COMMENT : str := (SLASH_CHARACTER, SLASH_CHARACTER, SLASH_CHARACTER, CHAR_A, CHAR_B, CHAR_A, CHAR_B, CHAR_A, CHAR_B);
-signal PART_LN_COMMENT: str := (CHAR_A, SLASH_CHARACTER, SLASH_CHARACTER, CHAR_A, CHAR_B, CHAR_A, NEW_LINE_CHARACTER, SLASH_CHARACTER, SLASH_CHARACTER);
+signal ALL_LN_COMMENT : str := (SLASH_CHARACTER, SLASH_CHARACTER, STAR_CHARACTER, CHAR_A, CHAR_B, CHAR_A, CHAR_B, NEW_LINE_CHARACTER, CHAR_B);
+signal PART_LN_COMMENT: str := (CHAR_A, SLASH_CHARACTER, SLASH_CHARACTER, STAR_CHARACTER, CHAR_B, CHAR_A, NEW_LINE_CHARACTER, CHAR_B, SLASH_CHARACTER);
 
 signal ALL_BLK_COMMENT: str := (SLASH_CHARACTER, STAR_CHARACTER, CHAR_A, CHAR_B, CHAR_A, NEW_LINE_CHARACTER, CHAR_B, STAR_CHARACTER, SLASH_CHARACTER);
-signal PART_BLK_COMMENT: str := (CHAR_A, CHAR_B, SLASH_CHARACTER, STAR_CHARACTER, CHAR_A, CHAR_B, STAR_CHARACTER, SLASH_CHARACTER, CHAR_B);
+signal PART_BLK_COMMENT: str := (CHAR_A, CHAR_B, SLASH_CHARACTER, STAR_CHARACTER, SLASH_CHARACTER, SLASH_CHARACTER, STAR_CHARACTER, SLASH_CHARACTER, CHAR_B);
 
 BEGIN
 dut: comments_fsm
@@ -56,24 +56,21 @@ END PROCESS;
 --TODO: Thoroughly test your FSM
 stim_process: PROCESS
 BEGIN    
-	REPORT "Example case, reading a meaningless character";
-	s_input <= "01011000";
-	WAIT FOR 1 * clk_period;
-	ASSERT (s_output = '0') REPORT "When reading a meaningless character, the output should be '0'" SEVERITY ERROR;
-	REPORT "_______________________";
-    
 	-- Making the entire string part of a comment
-	REPORT "Tests ALL_LN_COMMENT, which is a typical comment";
+	REPORT "Tests ALL_LN_COMMENT: `//*ABAB\nB`";
 	FOR i IN 8 DOWNTO 7 loop
 		s_input <= ALL_LN_COMMENT(i);
                 WAIT FOR 1 * clk_period;
 		ASSERT (s_output = '0') REPORT "Before Comment, should be 0" SEVERITY ERROR;	
 	END LOOP; --i
-	FOR i IN 6 DOWNTO 0 loop
+	FOR i IN 6 DOWNTO 1 loop
 		s_input <= ALL_LN_COMMENT(i);
                 WAIT FOR 1 * clk_period;
 		ASSERT (s_output = '1') REPORT "In Comment, should be 1" SEVERITY ERROR;
 	END LOOP; --i
+	s_input <= ALL_LN_COMMENT(0);
+	WAIT FOR 1 * clk_period;
+        ASSERT (s_output = '0') REPORT "After Comment, should be 0" SEVERITY ERROR;
 	REPORT "_______________________";
 
 	s_reset <= '1';
@@ -81,6 +78,7 @@ BEGIN
 	s_reset <= '0';
 	ASSERT (s_output = '0') REPORT "Couldn't Reset, should be '0'" SEVERITY ERROR;
 	
+	-- Simple tests, to make sure normal Code behaviour works
 	REPORT "Tests ALL_CODE, should stay '0'";
 	FOR i IN 8 DOWNTO 0 loop
 		s_input <= ALL_CODE(i);
@@ -94,7 +92,8 @@ BEGIN
 	s_reset <= '0';
 	ASSERT (s_output = '0') REPORT "Couldn't Reset, should be '0'" SEVERITY ERROR;
 
-	REPORT "Tests PART_LN_COMMENT. Some Code, Followed by a line comment, and then more code";
+	-- Ensures new line ends line comment
+	REPORT "Tests PART_LN_COMMENT. `A//ABAB\n/`";
 	FOR i IN 8 DOWNTO 6 loop
 		s_input <= PART_LN_COMMENT(i);
 		WAIT FOR 1 * clk_period;
@@ -117,7 +116,8 @@ BEGIN
 	s_reset <= '0';
 	ASSERT (s_output = '0') REPORT "Couldn't Reset, should be '0'" SEVERITY ERROR;
 
-	REPORT "Tests ALL_BLK_COMMENT, which is a typical comment";
+	-- Makes sure block comments work with normal comment headers inside
+	REPORT "Tests ALL_BLK_COMMENT `/*ABA\nB*/`";
 	FOR i IN 8 DOWNTO 7 loop
 		s_input <= ALL_BLK_COMMENT(i);
                 WAIT FOR 1 * clk_period;
@@ -135,7 +135,7 @@ BEGIN
 	s_reset <= '0';
 	ASSERT (s_output = '0') REPORT "Couldn't Reset, should be '0'" SEVERITY ERROR;
 
-	REPORT "Tests PART_BLK_COMMENT, code followed by comment, followed by code";
+	REPORT "Tests PART_BLK_COMMENT, `AB/*//*/B`";
 	FOR i IN 8 DOWNTO 5 loop
 		s_input <= PART_BLK_COMMENT(i);
 		WAIT FOR 1 * clk_period;
